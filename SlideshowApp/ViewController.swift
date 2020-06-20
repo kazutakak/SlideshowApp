3b569ad1d17da35d2aa03735c04d543fa21161c3
kazutakak <kitoukazutaka@gmail.com>
2020-06-20 18:47:15 +0900

    ramenをmenに変更いたしました。

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     var imageIndex = 0
 
     //表示する画像の配列定義
-    let images: [UIImage] = [UIImage(named:"kani.jpg")!, UIImage(named:"ramen.jpg")!, UIImage(named:"uni.jpg")!]
+    let images: [UIImage] = [UIImage(named:"kani.jpg")!, UIImage(named:"men.jpg")!, UIImage(named:"uni.jpg")!]
 
     override func viewDidLoad() {
         super.viewDidLoad()
